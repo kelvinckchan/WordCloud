0513700852c785d7e483244cec9451cebf472f5c
kelvi_000 <kelvinckchan94@gmail.com>
2017-12-20 17:03:48 +0800

    first

---
+++ wordcloud/src/main/java/com/wordcould/App.java
@@ -0,0 +1,36 @@
+package com.wordcould;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.io.IOException;
+import java.util.List;
+
+import com.kennycason.kumo.CollisionMode;
+import com.kennycason.kumo.WordCloud;
+import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.bg.CircleBackground;
+import com.kennycason.kumo.font.scale.SqrtFontScalar;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.ColorPalette;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        System.out.println( "Hello World!" );
+        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("text/text.txt");
+        final Dimension dimension = new Dimension(600, 600);
+        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+        wordCloud.setPadding(2);
+        wordCloud.setBackground(new CircleBackground(300));
+        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+        wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+        wordCloud.build(wordFrequencies);
+        wordCloud.writeToFile("output/datarank_wordcloud_circle_sqrt_font.png");
+    }
+}
